89e7b86891c8d7447f74cac10f1463e297aa050f
Pascal Ouimet <ouimepa@gmail.com>
2020-07-05 17:11:24 -0400

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,26 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-171570104-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "lindavaillancourtts.com",
+      }
+    }
   ],
 }
